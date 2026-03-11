a3f078d52d0bc7beee360e7c4eb4e535b15f8378
ridoluc <ridoluc@hotmail.it>
2026-03-11 15:32:25 +0000

    removed sw test files

--- sw/memory_test/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API package convenience imports."""
-
-from ..JTAGProg import JTAGProg, reconstruct_data_from_response, load_32bit_hex_file, ADDR_W
-
-__all__ = ["JTAGProg", "reconstruct_data_from_response", "load_32bit_hex_file", "ADDR_W"]
